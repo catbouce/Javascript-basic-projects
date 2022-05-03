e4f56a1f31bd67680c1fd00571adde393819c9a9
catbouce <catbouce@gmail.com>
2022-05-03 16:42:45 -0700

    till working on 14

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -2,14 +2,15 @@
 const groceryForm = document.querySelector('.grocery-form');
 const input = document.getElementById('grocery');
 const groceryContainer = document.querySelector('.grocery-container');
+const clearBtn = document.querySelector('.clear-btn');
 
 // edit option
 let edit = false;
-
+let inputFlag = "";
 // ****** EVENT LISTENERS **********
 //submit event
 groceryForm.addEventListener('submit',addItem);
-
+clearBtn.addEventListener('click',clearAll);
 //clear event
 
 // ****** FUNCTIONS **********
@@ -17,13 +18,17 @@ groceryForm.addEventListener('submit',addItem);
 function addItem(e){
     e.preventDefault();
     let inputValue = input.value;
-    // if edit is false and value not empty (add new grocerry item)
+    //if edit is false and value not empty (add new grocerry item)
     if (inputValue && !edit){
         // add a .grocery-item article dynamically
-        const groceryItem = document.createElement('grocery-item');
-        const id = new Date().getDay().toString();
+        const groceryItem = document.createElement('article');
+        const id = new Date().getTime().toString();
         //each article has unique data-id attribute 
-        
+        groceryItem.classList.add('grocery-item');
+        const dataAttr = document.createAttribute('data-id');
+        dataAttr.value = id;
+        groceryItem.setAttributeNode(dataAttr);
+        console.log(dataAttr);
         //update article's HTML to reflect the new added element
         groceryItem.innerHTML = `<p class="title">${inputValue}</p>
                                 <div class="btn-container">
@@ -37,6 +42,7 @@ function addItem(e){
                                 </button>
                                 </div>`;
         //append the new element to the list
+        groceryContainer.appendChild(groceryItem);
         //display alert, .sucess class
         //.show-container as it is hidden by default
         groceryContainer.classList.add('show-container');
@@ -58,14 +64,22 @@ function displayAlert(text){
     //remove alert after certain time
 
 }
-    
+
+const allGroceryItem = document.querySelectorAll('.grocery-item');
 //function to clear all items
+function clearAll(){
     //remove all the list item from the grocery list
+    allGroceryItem.forEach(function(item){
+        console.log(item);
+    })
     //hide the container/clear list button
     //display alert , .danger or sucess class
-    
+} 
 //function to set back to default
-
+function setToDefault(){
+    edit = false;
+    inputFlag = "";
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
